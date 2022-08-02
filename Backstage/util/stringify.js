5ce2eb8cd5ecd79441db2dea5eacfb845e8568ae
icey <icey@icey.tech>
2022-08-02 23:28:16 +0800

    改善 stringify

--- Backstage/util/stringify.js
+++ Backstage/util/stringify.js
@@ -2,7 +2,7 @@ const JSON5 = require('json5')
 
 const mapObject = object => {
    if (typeof object === 'function') {
-      return '[Function Code]'
+      return ' +*+ [ +*+ Function Code *+* ] +*+ '
    } else if (Array.isArray(object)) {
       return object.map(mapObject)
    } else if (typeof object === 'object') {
@@ -16,6 +16,15 @@ const mapObject = object => {
    }
 }
 
-const perfectStringify = object => JSON5.stringify(mapObject(object), { space: 3 })
+const escapeRegExp = string => string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+
+const replaceAll = (str, find, replace) => str.replace(new RegExp(escapeRegExp(find), 'g'), replace)
+
+const perfectStringify = object => {
+   let v = JSON5.stringify(mapObject(object), { space: 3 })
+   v = replaceAll(v, '\' +*+ [ +*+ Function Code *+* ] +*+ \'', '(gameContext, ...) => { ... }')
+   v = replaceAll(v, '\" +*+ [ +*+ Function Code *+* ] +*+ \"', '(gameContext, ...) => { ... }')
+   return v
+}
 
 module.exports = { perfectStringify }
