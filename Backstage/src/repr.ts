1ef61e8d1f6e6ed21271a552289c5e2eb3bc50ef
icey <icey@icey.tech>
2022-08-07 03:14:02 +0800

    可很久没深聊 也很久没拥抱 翻开书本把答案寻找

---
+++ Backstage/src/repr.ts
@@ -0,0 +1,84 @@
+import InternalTranslations from './representation/internal_translations'
+import {
+   HasSkillOrNot,
+   PotentialFunctionResult,
+   PotentialLogicOpResult,
+   PotentialResult,
+   SkillPotentialResult
+} from './executor/compute'
+
+export function translateInternalItem(
+   lang: string,
+   translations: Record<string, Record<string, string>>,
+   key: string
+) {
+   if (InternalTranslations[lang]) {
+      if (InternalTranslations[lang][key]) {
+         return InternalTranslations[lang][key]
+      }
+   }
+
+   if (translations[lang]) {
+      if (translations[lang][key]) {
+         return translations[lang][key]
+      }
+   }
+
+   console.warn(`[W] [translateInternalItem] missing translation for key '${key}'`)
+   return key
+}
+
+export function translateItem(
+   lang: string,
+   translations: Record<string, Record<string, string>>,
+   key: string
+) {
+   if (!key.startsWith('@')) {
+      return key
+   }
+
+   if (translations[lang]) {
+      if (translations[lang][key]) {
+         return translations[lang][key]
+      }
+   }
+
+   console.warn(`[W] [translateItem] missing translation for key '${key}'`)
+   return key
+}
+
+export function translatePotentialResult(
+   lang: string,
+   translations: Record<string, Record<string, string>>,
+   potentialResult: SkillPotentialResult[]
+) {
+   function impl(result: SkillPotentialResult): object {
+      if (result instanceof PotentialFunctionResult) {
+         return {
+            text: translateItem(lang, translations, result.description),
+            result: result.result
+         }
+      } else if (result instanceof PotentialLogicOpResult) {
+         return {
+            text: translateInternalItem(lang, translations, `@op_${result.op}`),
+            result: result.result,
+            arguments: result.resultPieces.map(impl)
+         }
+      } else if (result instanceof HasSkillOrNot) {
+         return {
+            text: translateInternalItem(lang, translations, '@broken_potential_result')
+         }
+      }
+
+      return null
+   }
+
+   if (potentialResult.length === 1) {
+      return impl(potentialResult[0])
+   } else {
+      return {
+         text: translateInternalItem(lang, translations, '@op_and'),
+         arguments: potentialResult.map(impl)
+      }
+   }
+}
