04ff6a408e8a8cba947d9122b727d8397701177e
CousinZe <alex@ckx-ily.cn>
2022-11-10 02:18:44 +0800

    尝试增加一点文档

--- backstage/src/ruleset/items/category.ts
+++ backstage/src/ruleset/items/category.ts
@@ -1,10 +1,25 @@
 import { MaybeTranslationKey } from '@app/base/uid'
 
-export type SkillCategoryId = string
+/// 活动类别 ID
+///
+/// **注意:** 不同于其他常见的 ID，活动类别 ID 被视为是“全局唯一”的，不会在被规则集加载时被名称重整
 export type ActivityCategoryId = string
 
+/// 技能类别 ID
+///
+/// **注意:** 不同于其他常见的 ID，技能类别 ID 被视为是“全局唯一”的，不会在被规则集加载时被名称重整
+export type SkillCategoryId = string
+
+/// 技能类别
+///
+/// 对应于原版游戏中的技能类别
 export interface SkillCategory {
+   /// 技能类别 ID
    readonly ident: SkillCategoryId
+
+   /// 技能类别名称
    readonly name: MaybeTranslationKey
+
+   /// 技能类别描述
    readonly description: MaybeTranslationKey
 }
--- backstage/src/ruleset/items/item_base.ts
+++ backstage/src/ruleset/items/item_base.ts
@@ -1,5 +1,6 @@
 import { Ident, MaybeTranslationKey, Scope } from '@app/base/uid'
 
+/// 暂未使用
 export type PatchMode = 'overwrite' | 'and' | 'or'
 
 export interface ItemBase {
