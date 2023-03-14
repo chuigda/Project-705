5148235ebe24c51f182ca3bf4479791504014d1c
Alex Greenall <alex@ckx-ily.cn>
2023-03-14 14:01:20 +0800

    export

--- backstage/rulesets/core_ruleset/event/activity.ts
+++ backstage/rulesets/core_ruleset/event/activity.ts
@@ -21,3 +21,5 @@ const activityEvents: Event[] = [
         }
     }
 ]
+
+export default activityEvents
--- backstage/rulesets/core_ruleset/event/index.ts
+++ backstage/rulesets/core_ruleset/event/index.ts
@@ -1,7 +1,9 @@
 import startupEvents from '@rulesets/core_ruleset/event/startup'
+import activityEvents from '@rulesets/core_ruleset/event/activity'
 import ascensionPerkEvents from '@rulesets/core_ruleset/event/ascension_perk'
 
 export default [
    ...startupEvents,
+   ...activityEvents,
    ...ascensionPerkEvents
 ]
